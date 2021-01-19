9b8e6c54f0b70a2727e92e07ad2327827c34056b
Avshalom <avshalom.tam@gmail.com>
2021-01-19 13:46:00 +0200

    added login and register pages

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('iddd')
+const express = require('express');
+
+const app = express();
+
+app.set('view-engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', { name: 'Avshalom' });
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (reg, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    
+})
+
+app.get('*', (req, res) => {
+    res.render('404Page.ejs')
+})
+
+app.listen(3000);
